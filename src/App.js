4f3f4a960981783e17b6ccd3ea7c9cab193dad98
Михаил Кормановский <kormanowsky@gmail.com>
2020-07-20 18:33:39 +0300

    RAdded go to bbuilder button

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ function App() {
     return (
         <div className="app app-home">
             <main>
-                <section class="container" id="section-home-top">
-                    <div class="row" id="home-top-row">
-                        <div class="col-xs-8" id="home-top-text">
+                <section className="container" id="section-home-top">
+                    <div className="row" id="home-top-row">
+                        <div className="col-xs-6" id="home-top-text">
                             <h1>Project 2</h1>
+                            <h3>Пиццерия в Чернево-2</h3>
                         </div>
-                        <div class="col-xs-4" id="home-top-pizza">
+                        <div className="col-xs-6" id="home-top-pizza">
                             <Pizza size={600} />
+                            <p style={{ textAlign: "center" }}>
+                                <a href="#/builder" className="button">
+                                    Собрать свою пиццу &raquo;
+                                </a>
+                            </p>
                         </div>
                     </div>
                 </section>
