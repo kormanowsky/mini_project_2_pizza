eb2a85dd87e8b01f58f3b061155816a5acebec0e
Михаил Кормановский <kormanowsky@gmail.com>
2020-07-22 02:02:03 +0300

    Added pizza image and pizza title to PizzaPage

--- src/components/pages/PizzaPage.js
+++ src/components/pages/PizzaPage.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Header from "../blocks/Header";
 import Footer from "../blocks/Footer";
+import { Pizza } from "../pizza/Pizza";
 
 class PizzaPage extends React.Component {
     render() {
         return (
             <div className="app app-pizza">
                 <Header />
-                <main>Pizza with ID {this.props.pizza.id}</main>
+                <main>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <Pizza
+                                    responsive={true}
+                                    toppings={this.props.pizza.toppings}
+                                />
+                            </div>
+                            <div class="col-xs-6">
+                                <h1>{this.props.pizza.name}</h1>
+                            </div>
+                        </div>
+                    </div>
+                </main>
                 <Footer />
             </div>
         );
