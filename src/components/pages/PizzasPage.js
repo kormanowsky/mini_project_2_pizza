c25ddb7ba7dd496f39a30bb51bc5c69cccc0ddd6
Михаил Кормановский <kormanowsky@gmail.com>
2020-07-26 00:53:01 +0300

    Add pizzas to pizzas page

--- src/components/pages/PizzasPage.js
+++ src/components/pages/PizzasPage.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../blocks/Header";
 import Footer from "../blocks/Footer";
+import Data from "../../data";
+import { Pizza } from "../pizza/Pizza";
+import { Link } from "react-router-dom";
+import Cart from "../../Cart";
 
 class PizzasPage extends React.Component {
     render() {
@@ -11,6 +15,37 @@ class PizzasPage extends React.Component {
                     <section id="section-pizzas">
                         <div className="container">
                             <h1 className="page-title">Пиццы</h1>
+                            <div className="row">
+                                {Data.pizzas.map((pizza) => (
+                                    <div
+                                        className="col-xs-12 col-md-6 col-lg-4 pizzas-pizza"
+                                        key={`pizza-${pizza.id}`}
+                                    >
+                                        <div className="row">
+                                            <div className="col-xs-12 col-md-6">
+                                                <Pizza
+                                                    toppings={pizza.toppings}
+                                                    responsive={true}
+                                                />
+                                            </div>
+                                            <div className="col-xs-12 col-md-6">
+                                                <h3>{pizza.name}</h3>
+                                                <p>{pizza.price} &#x20bd;</p>
+                                                <Link to={`/pizza/${pizza.id}`}>
+                                                    Подробнее
+                                                </Link>
+                                                <button
+                                                    onClick={() => {
+                                                        Cart.add(pizza);
+                                                        this.forceUpdate();
+                                                    }}
+                                                    className="button"
+                                                >В корзину</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </section>
                 </main>
