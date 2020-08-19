f777cea8579276f80bf9a78796593c7c45d91005
Михаил Кормановский <kormanowsky@gmail.com>
2020-08-19 20:48:13 +0300

    SCSS file for <Header>

--- src/components/blocks/Header.js
+++ src/components/blocks/Header.js
@@ -3,46 +3,48 @@ import Data from "../../data";
 import { Link } from "react-router-dom";
 import Cart from "../../Cart";
 
+import "../../scss/header.scss";
+
 class Header extends React.Component {
-    render() {
-        return (
-            <header>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xs-9" id="header-nav">
-                            <h3 className="font-pd">
-                                <Link to="/">{Data.projectInfo.name}</Link>
-                            </h3>
-                            <ul id="header-navigation">
-                                <li>
-                                    <Link to="/pizzas">Пиццы</Link>
-                                </li>
-                                <li>
-                                    <Link to="/builder">Конструктор</Link>
-                                </li>
-                                <li>
-                                    <Link to="/orders">Мои заказы</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col-xs-3" id="header-cart-info">
-                            <img
-                                src="/images/shopping-cart.svg"
-                                alt="Корзина"
-                                id="header-cart-icon"
-                            ></img>
-                            <p>
-                                <Link to="/cart">
-                                    {Cart.count} товаров на {Cart.total}
-                                    &#x20bd;
-                                </Link>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-9" id="header-nav">
+              <h3 className="font-pd">
+                <Link to="/">{Data.projectInfo.name}</Link>
+              </h3>
+              <ul id="header-navigation">
+                <li>
+                  <Link to="/pizzas">Пиццы</Link>
+                </li>
+                <li>
+                  <Link to="/builder">Конструктор</Link>
+                </li>
+                <li>
+                  <Link to="/orders">Мои заказы</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-xs-3" id="header-cart-info">
+              <img
+                src="/images/shopping-cart.svg"
+                alt="Корзина"
+                id="header-cart-icon"
+              ></img>
+              <p>
+                <Link to="/cart">
+                  {Cart.count} товаров на {Cart.total}
+                  &#x20bd;
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
