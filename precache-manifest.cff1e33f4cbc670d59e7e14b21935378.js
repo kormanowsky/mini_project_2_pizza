e4498002976df087df18cf65c9fd0cd91a53f186
Михаил Кормановский <kormanowsky@gmail.com>
2020-08-21 19:32:45 +0300

    Updates

---
+++ precache-manifest.cff1e33f4cbc670d59e7e14b21935378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5babd980ef00e658ec04100e4edc8efe",
+    "url": "/mini_project_2_pizza/index.html"
+  },
+  {
+    "revision": "cfad187dba1fa07605f8",
+    "url": "/mini_project_2_pizza/static/css/main.b0b4e32e.chunk.css"
+  },
+  {
+    "revision": "e5d7de6d6c2525d815ea",
+    "url": "/mini_project_2_pizza/static/js/2.c7c8a81c.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/mini_project_2_pizza/static/js/2.c7c8a81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfad187dba1fa07605f8",
+    "url": "/mini_project_2_pizza/static/js/main.be041720.chunk.js"
+  },
+  {
+    "revision": "dfc34621f32e006f1d7d",
+    "url": "/mini_project_2_pizza/static/js/runtime-main.18ae7b13.js"
+  }
+]);
